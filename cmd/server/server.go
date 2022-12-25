15c8651dad4d0cb985ee9a805549a01d196a9bc6
Thanapong Somjai <thanapongsj1996@gmail.com>
2022-12-26 02:07:49 +0700

    first commit

---
+++ cmd/server/server.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/subosito/gotenv"
+)
+
+func init() {
+	gotenv.Load()
+}
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":" + os.Getenv("PORT")))
+}
